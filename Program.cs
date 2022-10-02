775e18343ec3b9d678be041661cdebaeb3454169
Denis <disa.92@list.ru>
2022-10-02 21:53:16 +0700

    составили методы для решения задачи

--- Program.cs
+++ Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿string[] CreateArray(int text)
+{
+    
+    string[] array = new string[text];
+    for (int i = 0; i < text; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i=0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine("\b\b]");
+    
+}
+
+string[] ShowElements (string[] array)
+{
+    string [] newArray = new string[array.Length];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+             newArray[count] = array[i];
+             count++;
+        }
+    }
+    return newArray;
+} 
